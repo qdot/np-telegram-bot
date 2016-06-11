b81d7c8d54caf4e334e1cbe550223d9c112a6de5
Kyle Machulis <kyle@nonpolynomial.com>
2016-06-11 15:56:04 -0700

    Add setup.py file for doing pip installs

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='np-telegram-bot',
+      version='0.1',
+      description='NP Labs Telegram Bot Base Code',
+      author='Kyle Machulis',
+      author_email='kyle@machul.is',
+      url='https://github.com/qdot/np-telegram-bot/',
+      packages=["nptelegrambot"]
+     )
